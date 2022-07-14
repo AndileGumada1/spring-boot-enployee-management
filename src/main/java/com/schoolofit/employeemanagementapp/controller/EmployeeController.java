9f6d92122b344bc2d8034c29de0843781857beb1
andile <decemberandile@gmail.com>
2022-07-14 15:37:40 +0200

    fixed cors

--- src/main/java/com/schoolofit/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/schoolofit/employeemanagementapp/controller/EmployeeController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("https://react-employee-app-ui.herokuapp.com/")
 @RestController
 @RequestMapping("/api/employees")
 @RequiredArgsConstructor
